eac39622db0ac20f476e17886cb87d8a3484caf0
Kiefer Ragay <kiefer.m.ragay@gmail.com>
2020-05-22 19:50:18 -0700

    Create Template for Product CSV Writer

--- mongo/seedData/productCSVwriter.js
+++ mongo/seedData/productCSVwriter.js
@@ -1,10 +1,31 @@
+const path = require('path');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const imageSetMaker = require('./imageSetMaker.js');
+
 const csvWriter = createCsvWriter({
-  path: path.join(__dirname, 'images.csv'),
+  path: path.join(__dirname, 'products.csv'),
   header: [
-    { id: 'image_url', title: 'image_url' },
+    { id: 'product_name', title: 'product_name' },
     { id: 'views', title: 'views' },
-    { id: 'height', title: 'height' },
-    { id: 'width', title: 'width' },
     { id: 'date_added', title: 'date_added'}
+    { id: 'images', title: 'images'}
   ]
 });
+
+const records = [];
+
+for (let i = 0; i < 1000000; i += 1) {
+  let newRecord = {};
+  newRecord.views = Math.floor(Math.random() * (100000 - 27) + 27);
+  const randomYear = Math.floor(Math.random() * (2021 - 2012) + 2012);
+  const randomMonth = Math.floor(Math.random() * 12);
+  const randomDay = Math.floor(Math.random() * 32);
+  newRecord.date_added = new Date(randomYear, randomMonth, randomDay);
+  newRecord.images = imageSetMaker(Math.floor(Math.random() * (16 - 5) + 5);
+  records.push(newRecord);
+};
+
+csvWriter.writeRecords(records)
+  .then(() => {
+    console.log('...Done');
+  });
