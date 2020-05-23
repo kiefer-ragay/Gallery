b5b628ebf1977cad1ff8c6c679f4068f78d87426
Kiefer Ragay <kiefer.m.ragay@gmail.com>
2020-05-23 00:26:50 -0700

    Attempt Promisifying Seed Batches

--- mongo/seedData/productSeeder.js
+++ mongo/seedData/productSeeder.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Product = require('../schema.js').Product;
 const nameGenerator = require('./productNameGenerator.js');
 const imageSetMaker = require('./imageSetMaker.js');
-const Promise = require('bluebird');
+const promise = require('bluebird');
 
 mongoose.connect('mongodb://localhost/gallery', { useNewURLParser: true });
 
@@ -12,16 +12,21 @@ db.on('error', console.error.bind(console, 'connection error:'));
 
 db.once('open', () => {
   console.log('Connected to Gallery, seeding...');
-  promisedSeeder(0, 100000)
-    .then(promisedSeeder(100001, 200000).delay(60000))
-    .then(promisedSeeder(200001, 300000).delay(60000))
-    .then(promisedSeeder(300001, 400000).delay(60000))
-    .then(promisedSeeder(400001, 500000).delay(60000))
-    .then(promisedSeeder(500001, 600000).delay(60000))
-    .then(promisedSeeder(600001, 700000).delay(60000))
-    .then(promisedSeeder(700001, 800000).delay(60000))
-    .then(promisedSeeder(800001, 900000).delay(60000))
-    .then(promisedSeeder(900001, 999999).delay(60000))
+
+  let chain = promise.resolve();
+  for (let i = 0; i <= 950000; i += 50000) {
+    chain = chain.then(promisedSeeder(i, i + 50000)).then(Wait());
+  }
+  // promisedSeeder(0, 100000)
+  //   .then(promisedSeeder(100001, 200000).delay(60000))
+  //   .then(promisedSeeder(200001, 300000).delay(60000))
+  //   .then(promisedSeeder(300001, 400000).delay(60000))
+  //   .then(promisedSeeder(400001, 500000).delay(60000))
+  //   .then(promisedSeeder(500001, 600000).delay(60000))
+  //   .then(promisedSeeder(600001, 700000).delay(60000))
+  //   .then(promisedSeeder(700001, 800000).delay(60000))
+  //   .then(promisedSeeder(800001, 900000).delay(60000))
+  //   .then(promisedSeeder(900001, 999999).delay(60000))
 
 });
 
@@ -46,4 +51,7 @@ const productSeeder = (start, stop) => {
   console.log(`Seeding ${start} to ${stop} Complete`);
 }
 
-const promisedSeeder = Promise.promisify(productSeeder);
+const promisedSeeder = promise.promisify(productSeeder);
+function Wait() {
+  return new Promise(r => setTimeout(r, 30000));
+}
