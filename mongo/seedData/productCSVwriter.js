b84c12aa37b8b3a76d6ac5079711869e50aebaae
Kiefer Ragay <kiefer.m.ragay@gmail.com>
2020-05-22 22:12:12 -0700

    Move Product Name Generator to Own File

--- mongo/seedData/productCSVwriter.js
+++ mongo/seedData/productCSVwriter.js
@@ -1,41 +1,9 @@
 const path = require('path');
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 const imageSetMaker = require('./imageSetMaker.js');
+const nameGenerator = require('./productNameGenerator');
 
-const adjectives = [
-  'Frumpy', 'Pure', 'Outdated', 'Tasteless', 'Tasteful',
-  'Melted', 'Arrogant', 'Dusty', 'Immense', 'Disproportionate',
-  'Colossal', 'Ginormous', 'Mediocre', 'Puny', 'Average', 'Ugly',
-  'Prickly', 'Broken', 'Filthy', 'Defiant', 'Ill-Conceived',
-  'Wet', 'Magnificent', 'Fabulous', 'Poorly-Constructed',
-  'Just Okay', 'Used', 'Mysterious', "Rambunctious", 'Fergilicious',
-  'Giant', 'Aggressive', 'Triumphant', 'Fake', 'Expensive', 'Rusty'
-];
 
-const colors = [
-  'Mauve', 'Flesh-Colored', 'Brown', 'Magenta', 'Rainbow',
-  'Pink', 'Flesh-Colored', 'Red', 'Razzmatazz', 'Periwinkle',
-  'Sage', 'Fushcia', 'Strawberry Red', 'Lipstick Green', 'Pickle-Colored',
-  'Butterscotch Yellow', 'Crusty Brown', 'Fancy Green', 'Bronze', 'Blue',
-  'Lemon Zest Yellow', 'Raisin Purple', 'Musty Orange', 'Mahogany'
-];
-
-const toys = [
-  'Fergie', 'Bat', 'Obi Wan Kenobi', 'Star Wars', 'Land Before Time',
-  'Indiana Jones', 'Madonna', 'Dinosaur', 'Oompa Loompa', 'President Trump',
-  'Manhattan City', 'Orangutan', 'Motorcycle', 'Minions', 'Demon Bull King', 'Pigsy Food Truck',
-  'Beanstalk', 'Monkey King', 'Princess Peach', 'Pikachu', 'Bulbasaur', 'Pokemon',
-  'Millenial', 'Gen Z', 'Boomer', 'Amelia Lockhart', 'Darth Vader', 'R2-D2',
-  'Jar Jar Binks', 'Wookie', 'Catwoman', 'Catman', 'Batman', 'Teen Titans',
-  'Tween Titans', 'My Sweet 16', 'Newborne Child', 'Hilary', 'Karen', 'Racecar',
-  'Orc', 'Gnome', 'Construction Man', 'Fat Chocobo', 'Wolverine', 'Jean Gray'
-];
-
-const setNames = [
-  'Piece', 'Collection', 'Diorama', 'Set', 'Thing',
-  'Piece', 'Collection', 'Diorama', 'Set', 'Assemblage',
-  'Assortment'
-];
 
 
 
@@ -56,8 +24,7 @@ const records = [];
 
 for (let i = 0; i < 5; i += 1) {
   let newRecord = {};
-  newRecord.product_name = adjectives[Math.floor(Math.random() * adjectives.length)] + ' ' +
-    colors[Math.floor(Math.random() * colors.length)] + ' ' + toys[Math.floor(Math.random() * toys.length)] + ' ' + setNames[Math.floor(Math.random() * setNames.length)];
+  newRecord.product_name =
 
   newRecord.views = Math.floor(Math.random() * (100000 - 27) + 27);
   const randomYear = Math.floor(Math.random() * (2021 - 2012) + 2012);
--- mongo/seedData/productSeeder.js
+++ mongo/seedData/productSeeder.js
@@ -1,12 +1,34 @@
 const mongoose = require('mongoose');
 const Product = require('../schema.js').Product;
 
-mongoose.connect('mongodb://localhost/gallery', { useNewURLParser: true }
-);
+mongoose.connect('mongodb://localhost/gallery', { useNewURLParser: true });
 
 const db = mongoose.connection;
 
+db.on('error', console.error.bind(console, 'connection error:'));
+
 db.once('open'), () => {
+  console.log('Connected to Gallery, seeding...');
 
 }
 
+const productSeeder = () => {
+  for ( let i = 0; i < 5; i ++) {
+    const randomYear = Math.floor(Math.random() * (2021-2012) + 2012);
+    const randomMonth = Math.floor(Math.random() * 12);
+    const randomDay = Math.floor(Math.random() * 32);
+    const ProductEntry = new Product({
+      _id: ObjectId(imageIds[i]),
+      image_url: images[i].image_url,
+      views: Math.floor(Math.random() * (100000 - 27) + 27),
+      height: Math.floor(Math.random() * (2001 - 400) + 400),
+      width: Math.floor(Math.random() * (2001 - 400) + 400),
+      date_added: new Date(randomYear, randomMonth, randomDay)
+    });
+    ImageEntry.save((err, value) => {
+      if (err) {
+        console.log(err)
+      }
+    });
+  }
+}
