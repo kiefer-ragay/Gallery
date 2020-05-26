6d6958941e9eba5228f7dbc4e969030da39f0319
Kiefer Ragay <kiefer.m.ragay@gmail.com>
2020-05-26 15:08:24 -0700

    Return referenceCSVwriter to Original Form

--- postgres/seedData/referenceCSVwriter.js
+++ postgres/seedData/referenceCSVwriter.js
@@ -1,17 +1,17 @@
 const path = require('path');
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 const imageSetMaker = require('./imageSetMaker.js');
-
-
+const fs = require('fs');
 
 const csvWriter = createCsvWriter({
-  path: path.join(__dirname, 'references.csv'),
+  path: path.join(__dirname, 'references4.csv'),
   header: [
     { id: 'reference_id', title: 'reference_id' },
     { id: 'product_id', title: 'product_id' },
     { id: 'image_id', title: 'image_id' }
   ]
 });
+
 
 const writeToCSV = (start, end) => {
 
@@ -22,8 +22,8 @@ const writeToCSV = (start, end) => {
     let imageSet = imageSetMaker(Math.floor(Math.random() * (15-7) + 7));
     for (imageId of imageSet) {
       let newRecord = {};
-      newRecord.product_id = i;
       newRecord.reference_id = referenceCounter;
+      newRecord.product_id = i;
       newRecord.image_id = imageId;
       records.push(newRecord);
       referenceCounter += 1;
@@ -33,12 +33,9 @@ const writeToCSV = (start, end) => {
   csvWriter.writeRecords(records)
     .then(() => {
       console.log(`${start} to ${end}...Done!`);
-      Promise.resolve();
     });
 };
 
 
-Promise.resolve()
-promisedWrite(0, 100000)
-.then( () => promisedWrite(100001, 200000))
-.catch(console.log('Error Writing'));
+writeToCSV(7500001, 10000000);
+
