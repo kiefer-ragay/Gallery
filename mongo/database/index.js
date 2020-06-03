af180152e9a4aa8ea210194b9757f206de9a2f37
Kiefer Ragay <kiefer.m.ragay@gmail.com>
2020-06-03 12:27:09 -0700

    Refactor Database Index to Use dotenv

--- mongo/database/index.js
+++ mongo/database/index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
+
 const MongoClient = require('mongodb').MongoClient;
 
-const url = 'mongodb://localhost/:27017';
+const url = process.env.GALLERY_DATABASE;
 
 module.exports = (callback) => {
    MongoClient.connect(url, { poolSize: 15 }, (err, client) => {
