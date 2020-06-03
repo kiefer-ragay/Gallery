8246c998f121c0b7c48aac4a11067ae279aeea20
Kiefer Ragay <kiefer.m.ragay@gmail.com>
2020-06-02 19:17:49 -0700

    Update Pool Number

--- mongo/database/index.js
+++ mongo/database/index.js
@@ -1,11 +1,9 @@
 const MongoClient = require('mongodb').MongoClient;
 
 const url = 'mongodb://localhost/:27017';
-const dbName = 'gallery';
-const collectionName = 'products';
 
 module.exports = (callback) => {
-   MongoClient.connect(url, { poolSize: 5 }, (err, client) => {
+   MongoClient.connect(url, { poolSize: 10 }, (err, client) => {
     if (err) {
       console.log(err)
     } else {
